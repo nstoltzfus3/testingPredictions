5a132bb0e15504eaa11f6d51d84f0f5c9f29e765
Nicholas Stoltzfus <nstoltzfus3@gatech.edu>
2015-05-08 02:46:53 -0400

    Updated Archer module with comments.

--- testsforAPP/src/edu/udel/nstoltz/Predictionz/Archer.java
+++ testsforAPP/src/edu/udel/nstoltz/Predictionz/Archer.java
@@ -4,9 +4,19 @@ import java.util.ArrayList;
 
 import edu.udel.jatlas.gameframework.Position;
 
+/**
+ * Ranged Unit - Current stats:
+ * Cost:	100
+ * Health:	30
+ * Damage:	25
+ * Move:	3
+ * AOE:		cardinal directions 2-3 spaces away in an area.
+*/
 public class Archer extends Unit{
 	
-	
+	/**
+	 * Different constructors for convenience. Arbitrary placement of 5/5 for random archer testing.
+	 */
 	public Archer(Player player) {
 		this(new Position(5,5), player);
 	}
@@ -23,14 +33,32 @@ public class Archer extends Unit{
 		return "Archer";
 	}
 	
+	/**
+	 * Will return A1,A2,A3. Also the symbols used in the textual representation of the game.
+	 */
 	public String toString() {
 		return "A" + this.getOwner().getPlayerNumber();
 	}
 	
+	/**
+	 * Used to reset the archers move points at the end of turn to maximum
+	 * (Other implementations for icing or slowing effects can be added in by setting movePoints to Max/2 or Max/3 rounded.)
+	 */
 	public int getMaxMovePoints() {
 		return 3;
 	}
 	
+	
+	/**
+	 * Params:
+	 * char direction (n,s,e,w)
+	 * Returns the area of space that the archer hits at, 
+	 * currently an area that is 2 and 3 spaces away from the archer in line with the direction.
+	 * 
+	 * Example: Archer shoots 'e', DIRECTION_RIGHT
+	 * _______________
+	 * A1..)))).......
+	 */
 	public ArrayList<Position> getAttackArea(char direction) {
 		this.setDirection(direction);
 		int y = this.getPosition().getRow();
@@ -56,6 +84,7 @@ public class Archer extends Unit{
 				attackArea.add(new Position(i, y));
 			}
 		}
+//Used for testing purposes:
 //		for (Position e : attackArea) {
 //			System.out.println(e);
 //		}
